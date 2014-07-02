e478edc34bfef4a2894a11e92ff7202c9a2f9a3c
wyldphyre <craigreynolds@Claire.local>
2014-07-02 15:48:59 +0800

    Initial bookmarklet commit

---
+++ DuplicateTab.js
@@ -0,0 +1,6 @@
+/*
+  Author: Craig Reynolds
+  Description: Duplicate a tab. For browsers that don't have a built in duplicate feature
+*/
+
+javascript:(function(){window.open(window.location.href);})()
